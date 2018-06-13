0be64893cd69b7b42a53698bb25155dfa0b5d624
Thomas Marangoni <thomas.marangoni@hotmail.com>
2018-06-13 20:31:27 +0200

    First push

---
+++ FIB/roof/FIB-roof.gt-mp.cs
@@ -0,0 +1,20 @@
+// Objects (Total: 17)
+API.createObject(-505101878, new Vector3(126.43, -740.07, 265.75), new Vector3(0, -0, 160.000824), 0);
+API.createObject(-2033615398, new Vector3(121.39, -745.6, 264.5967), new Vector3(5.008955E-06, -5.00895567E-06, 49.9999962), 0);
+API.createObject(-2033615398, new Vector3(119.02, -740.34, 264.59), new Vector3(-4.46236E-05, 2.23117986E-05, 11.9999952), 0);
+API.createObject(-2033615398, new Vector3(120.9, -735.03, 264.5962), new Vector3(-4.462359E-05, -6.329292E-05, 139.999985), 0);
+API.createObject(-2033615398, new Vector3(126.041908, -732.64, 264.590363), new Vector3(5.97114047E-13, -5.00895567E-06, 89.99999), 0);
+API.createObject(-2033615398, new Vector3(133.77, -739.69, 264.595367), new Vector3(0, 0, 4.00002527), 0);
+API.createObject(-2033615398, new Vector3(126.73, -747.53, 264.603149), new Vector3(6.3292915E-05, -6.329289E-05, 89.99998), 0);
+API.createObject(-113824571, new Vector3(127.452293, -762.99, 261.8186), new Vector3(0, -0, 160.000687), 0);
+API.createObject(-2033615398, new Vector3(131.432953, -734.53, 264.590363), new Vector3(-4.46235972E-05, -6.329294E-05, 49.9999962), 0);
+API.createObject(-568951729, new Vector3(126.03, -732.64, 261.68), new Vector3(5.97114047E-13, -5.00895567E-06, 89.99999), 0);
+API.createObject(-568951729, new Vector3(131.43, -734.52, 261.64), new Vector3(1.001783E-05, -5.00895567E-06, -39.25), 0);
+API.createObject(-568951729, new Vector3(133.77, -739.69, 261.66), new Vector3(1.00177558E-05, 5.008955E-06, -85.49999), 0);
+API.createObject(-568951729, new Vector3(120.9, -735.03, 261.68), new Vector3(1.00178859E-05, -5.00895567E-06, 48.99917), 0);
+API.createObject(-568951729, new Vector3(119.02, -740.34, 261.68), new Vector3(-4.46236045E-05, 2.23118022E-05, 11.000001), 0);
+API.createObject(-568951729, new Vector3(121.38, -745.61, 261.68), new Vector3(6.329291E-05, -6.329292E-05, 49.0899925), 0);
+API.createObject(-568951729, new Vector3(126.72, -747.52, 261.68), new Vector3(6.329267E-05, -6.329293E-05, -0.41006583), 0);
+API.createObject(-1543048205, new Vector3(147.05, -734.375854, 261.91), new Vector3(1.00177713E-05, 5.00895658E-06, 70.99943), 0);
+// Markers (Total: 1)
+API.createMarker(1, new Vector3(141.086349, -735.543152, 261.851257), new Vector3(), new Vector3(0, 0, 0), new Vector(0.75, 0.75, 0.75), 255, 255, 0, 255, 0, false);
